05e4bd66a10f9f522428a8f1fb6b06b83d173522
Adrian <reinieradrianluna@gmail.com>
2023-07-12 22:07:04 +0800

    do a booking v1(fully functioning)

--- try.cpp
+++ try.cpp
@@ -1,30 +1,44 @@
-#include<iostream>
-using namespace std;
-
- void roomPanels(){
-
+void receipt() {
+    system("cls");
     std::cout<<R"(
 
-                    ==============    ==============    ==============    ==============    ==============             
-                    ||          ||    ||          ||    ||          ||    ||          ||    ||          ||
-                    ||    1     ||    ||    2     ||    ||    3     ||    ||    4     ||    ||    5     ||
-                    ||          ||    ||          ||    ||          ||    ||          ||    ||          ||
-                    ==============    ==============    ==============    ==============    ==============
 
-
-                    ==============    ==============    ==============    ==============    ==============
-                    ||          ||    ||          ||    ||          ||    ||          ||    ||          ||
-                    ||    6     ||    ||    7     ||    ||    8     ||    ||    9     ||    ||    10    ||
-                    ||          ||    ||          ||    ||          ||    ||          ||    ||          ||
-                    ==============    ==============    ==============    ==============    ==============
-
+                                              
+                                                                ______     ______     ______     ______     __     ______   ______  
+                                                                /\  == \   /\  ___\   /\  ___\   /\  ___\   /\ \   /\  == \ /\__  _\ 
+                                                                \ \  __<   \ \  __\   \ \ \____  \ \  __\   \ \ \  \ \  _-/ \/_/\ \/ 
+                                                                \ \_\ \_\  \ \_____\  \ \_____\  \ \_____\  \ \_\  \ \_\      \ \_\ 
+                                                                \/_/ /_/   \/_____/   \/_____/   \/_____/   \/_/   \/_/       \/_/ 
+                                                                     
 
 
     )"<<std::endl;
-}
+    cout << "\n\n\t\t\t\tRECEIPT" << endl;
+    cout << "\t\t\t--------------------------------" << endl;
 
+    int numBookings = 0;  // Counter for the number of bookings made in the current transaction
+    Node* curr = head;    // Starting node for the current transaction
 
-int main(){
+    while (curr != nullptr) {
+        numBookings++;
+        cout << "\t\t\tNo. Of individuals: " << curr->noInd << endl;
+        cout << "\t\t\tName of the booker: " << curr->name << endl;
+        cout << "\t\t\tNO. of bed to be used: " << curr->noBed << endl;
+        cout << "\t\t\tStaying time: " << curr->stayTime << endl;
+        cout << "\t\t\tRoom Category: " << opt2 << endl;
+        cout << "\t\t\tRoom Number: " << curr->roomChoice << endl;
 
-    roomPanels();
+        int x = stoi(curr->stayTime);
+        int cost = x * 350;
+        cout << "\t\t\tTotal cost: " << cost << endl;
+
+        cout << endl;
+        curr = curr->next;
+    }
+
+    cout << "\n\t\t\t--------------------------------" << endl;
+    cout << "\t\t\tNumber of Bookings: " << numBookings << endl;
+    cout << "\n\n\nPress any key to continue....";
+    cin.ignore();
+    cin.get();
 }
