4f7f34d49a769325eb05bd9d1a82015e5748f100
Adrian <reinieradrianluna@gmail.com>
2023-07-15 12:37:11 +0800

    modify booking is now working

--- try.cpp
+++ try.cpp
@@ -1,39 +1,80 @@
-void receipt() {
-    std::cout<<R"(
-
-
-                                              
-                                                                ______     ______     ______     ______     __     ______   ______  
-                                                                /\  == \   /\  ___\   /\  ___\   /\  ___\   /\ \   /\  == \ /\__  _\ 
-                                                                \ \  __<   \ \  __\   \ \ \____  \ \  __\   \ \ \  \ \  _-/ \/_/\ \/ 
-                                                                 \ \_\ \_\  \ \_____\  \ \_____\  \ \_____\  \ \_\  \ \_\      \ \_\ 
-                                                                  \/_/ /_/   \/_____/   \/_____/   \/_____/   \/_/   \/_/       \/_/ 
-                                                                     
-
-
-    )"<<std::endl;
-    system("cls");
-    cout << "\n\n\t\t\t\t\t\t\t\tRECEIPT" << endl;
-    cout << "\t\t\t\t\t\t\t\t--------------------------------" << endl;
-
-    // Read and display the receipt from the specific transaction's text file
-    auto timestamp = chrono::system_clock::now();
-    auto timestampSeconds = chrono::duration_cast<chrono::seconds>(timestamp.time_since_epoch()).count();
-    string filename = "booking_" + to_string(timestampSeconds) + ".txt";
-
-    ifstream inFile(filename);
-    if (inFile.is_open()) {
-        string line;
-        while (getline(inFile, line)) {
-            cout << "\t\t\t\t\t\t\t\t" << line << endl;
-        }
-
-        inFile.close();
-    } else {
-        cout << "Unable to open receipt file for reading." << endl;
+void ModifyFile(const string& filename, const string& username) {
+    ifstream inputFile(filename);
+    if (!inputFile) {
+        cout << "Error opening file: " << filename << endl;
+        return;
     }
 
-    cout << "\n\n\t\t\t\t\tPress any key to continue....";
-    cin.ignore();
-    cin.get();
+    vector<string> lines;
+    string line;
+    bool modified = false;
+
+    while (getline(inputFile, line)) {
+        // Find the line containing the user's name
+        if (line.find("Name of the booker: ") != string::npos && line.find(username) != string::npos) {
+            modified = true;
+
+            // Modify the user's information
+            cout << "Enter the new number of individuals: ";
+            getline(cin, line);
+            lines.push_back("No. Of individuals: " + line);
+
+            cout << "Enter the new number of beds: ";
+            getline(cin, line);
+            lines.push_back("NO. of bed to be used: " + line);
+
+            cout << "Enter the new staying time: ";
+            getline(cin, line);
+            lines.push_back("Staying time: " + line);
+
+            // Skip the existing room choice line
+            getline(inputFile, line);
+
+            cout << "Enter the new room category: ";
+            getline(cin, line);
+            lines.push_back("Room Category: " + line);
+
+            cout << "Enter the new room number: ";
+            getline(cin, line);
+            lines.push_back("Room Number: " + line);
+
+            // Skip the existing total cost line
+            getline(inputFile, line);
+        } else {
+            lines.push_back(line);
+        }
+    }
+    inputFile.close();
+
+    if (!modified) {
+        cout << "User not found in the file." << endl;
+        return;
+    }
+
+    ofstream outputFile(filename);
+    if (!outputFile) {
+        cout << "Error opening file for writing: " << filename << endl;
+        return;
+    }
+
+    // Write the modified lines back to the file
+    for (const string& modifiedLine : lines) {
+        outputFile << modifiedLine << endl;
+    }
+    outputFile.close();
+
+    cout << "File modified successfully." << endl;
+}
+
+int main() {
+    // Example usage
+    string filename = "allbookings.txt";
+    string username;
+
+    cout << "Enter the username to modify: ";
+    getline(cin, username);
+
+    ModifyFile(filename, username);
+
+    return 0;
 }
